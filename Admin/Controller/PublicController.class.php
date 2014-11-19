7731dc7403e2b156e4310b025a0af6d2a0d4c0ee
tonbo <tonbochow@qq.com>
2014-11-19 23:09:50 +0800

    登陆修改

--- Admin/Controller/PublicController.class.php
+++ Admin/Controller/PublicController.class.php
@@ -18,8 +18,16 @@ class PublicController extends Controller {
                 $data['errorVerifycode'] = '验证码错误';
                 $this->ajaxReturn($data);
             }
-            $data['message'] = '用户名或密码错误';
-            $this->ajaxReturn($data);
+            //检索用户是否存在
+            $userModel = M('User');
+            $user_cond['name'] = $name;
+            $user_cond['password'] = md5($password);
+            $user = $userModel->where($user_cond)->find();
+            if (!$user) {
+                $data['message'] = '用户名或密码错误';
+                $this->ajaxReturn($data);
+            }
+            $this->redirect('/Admin/Index/index');
         }
         $this->assign('verify_img', $verify_img);
         $this->assign('name', 'admin');

e4ea1354d09da36378c22cd4b4844a0a8615aa23
tonbo <tonbochow@qq.com>
2014-11-19 17:33:52 +0800

    后台登录页面

--- Admin/Controller/PublicController.class.php
+++ Admin/Controller/PublicController.class.php
@@ -8,6 +8,19 @@ class PublicController extends Controller {
 
     //后台登录页面
     public function Login() {
+        if (IS_POST) {
+            $name = I('post.name');
+            $password = I('post.password');
+            $verifycode = I('post.verifycode');
+            //检验验证码
+            $check_verifycode = $this->checkVerify($verifycode);
+            if (!$check_verifycode) {
+                $data['errorVerifycode'] = '验证码错误';
+                $this->ajaxReturn($data);
+            }
+            $data['message'] = '用户名或密码错误';
+            $this->ajaxReturn($data);
+        }
         $this->assign('verify_img', $verify_img);
         $this->assign('name', 'admin');
         $this->display('login');
@@ -20,8 +33,9 @@ class PublicController extends Controller {
     }
 
     //验证验证码
-    public function checkVerify() {
-        
+    public function checkVerify($verifycode) {
+        $verify = new \Think\Verify();
+        return $verify->check($verifycode);
     }
 
 }

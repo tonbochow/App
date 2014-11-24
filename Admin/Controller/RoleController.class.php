2d665141008c78e85ffb527f48d7dd00d675f41e
tonbo <tonbochow@qq.com>
2014-11-24 17:32:43 +0800

    后台角色修改

--- Admin/Controller/RoleController.class.php
+++ Admin/Controller/RoleController.class.php
@@ -7,7 +7,13 @@ class RoleController extends BaseController{
     public function index(){
         $roleModel = M('Role');
         $roles = $roleModel->select();
+        $role_count = $roleModel->count();
+        import('Common.Extends.Page.BootstrapPage');
+        $Page = new \BootstrapPage($role_count, 1);
+        $roles = $roleModel->limit($Page->firstRow . ',' . $Page->listRows)->select();
 
+        $show = $Page->show(); // 分页显示输出
+        $this->assign('page',$show);
         $this->assign('roles',$roles);
         $this->display('index');
     }
@@ -32,6 +38,21 @@ class RoleController extends BaseController{
     
     //角色编辑
     public function edit(){
+        if(IS_POST){
+            $roleModel = D('Role');
+            $data = I('post.');
+            $data['status'] = I('post.status')['id'];
+            $res = $roleModel->create($data);
+            if($res){
+                $roleModel->save();
+                $data['status'] = true;
+                $data['success'] = '编辑成功';
+            }else{
+                $data['message'] = $roleModel->getError();
+                $data['status'] = false;
+            }
+            $this->ajaxReturn($data);
+        }
         $role_id = I('get.id');
         $role = M('Role')->where(array('id'=>$role_id))->find();
         if(empty($role)){
@@ -43,7 +64,43 @@ class RoleController extends BaseController{
     
     //角色删除
     public function delete(){
-        
+        $role_id = I('post.role_id');
+        $role = M('Role')->where(array('id'=>$role_id))->find();
+        if(empty($role)){
+            $this->error('未检索到该角色');
+        }
+        //删除role角色表数据
+        $roleModel = M('Role');
+        $roleModel->startTrans();
+        $role_data['id'] = $role_id;
+        $role = $roleModel->where($role_data)->delete();
+        if(!$role){
+            $roleModel->rollback();
+            $data['status'] = false;
+            $data['message'] = '删除角色表数据失败';
+        }
+        //删除role_use 角色用户表数据
+        $roleUserModel = M('RoleUser');
+        $roleUser_data['role_id'] = $role_id;
+        $role_user = $roleUserModel->where($roleUser_data)->delete();
+        if($role_user === false){
+            $roleModel->rollback();
+            $data['status'] = false;
+            $data['message'] = '删除role_user表数据失败';
+        }
+        //删除access权限表数据
+        $accessModel = M('Access');
+        $access_data['role_id'] = $role_id;
+        $access = $accessModel->where($access_data)->delete();
+        if($access === false){
+            $roleModel->rollback();
+            $data['status'] = false;
+            $data['message'] = '删除access表数据失败';
+        }
+        $data['status'] = true;
+        $data['success'] = '删除成功';
+        $roleModel->commit();
+        $this->ajaxReturn($data);
     }
 }
 ?>

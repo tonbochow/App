bef50b6f6a43a7209bb041c988297637f570db6e
tonbo <tonbochow@qq.com>
2014-11-25 22:56:26 +0800

    后台app 功能修改

--- Admin/Controller/AppController.class.php
+++ Admin/Controller/AppController.class.php
@@ -5,28 +5,31 @@ namespace Admin\Controller;
 use Think\Controller;
 
 class AppController extends BaseController {
-  
+
     //应用管理
-    public function index(){
-        
+    public function index() {
+
         $this->display('index');
     }
-    
+
     //添加应用
-    public function add(){
-        
+    public function add() {
+        if (IS_POST) {
+            
+        }
         $this->display('add');
     }
-    
+
     //编辑应用
-    public function edit(){
+    public function edit() {
         
     }
-    
+
     //删除应用
-    public function delete(){
+    public function delete() {
         
     }
+
 }
 
 ?>

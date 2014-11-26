1fc98d9e269511e08e56634b7a22992b279fa920
tonbo <tonbochow@qq.com>
2014-11-26 23:01:12 +0800

    角色应用功能

--- Admin/Controller/RoleAppController.class.php
+++ Admin/Controller/RoleAppController.class.php
@@ -11,6 +11,33 @@ class RoleAppController extends BaseController {
         
         $this->display('index');
     }
+    
+    //添加角色应用
+    public function add(){
+        $roleModel = M('Role');//检索所有角色
+        $roles = $roleModel->select();
+        $role_select = array();
+        if(!empty($roles)){
+            foreach($roles as $role){
+                $role_select[]= array('role_id'=>$role['id'],'role_name'=>$role['name']);
+            }
+        }
+        $appModel = M('App');//检索所有应用
+        $apps = $appModel->select();
+        $this->assign('apps',$apps);
+        $this->assign('role_select',  json_encode($role_select));
+        $this->display('add');
+    }
+    
+    //编辑角色应用
+    public function edit(){
+        
+    }
+    
+    //删除角色应用
+    public function delete(){
+        
+    }
 }
 
 ?>

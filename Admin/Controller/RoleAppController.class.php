b8d364dc3ed6983b2ad4b87955c8ab50584e95cd
tonbo <tonbochow@qq.com>
2014-11-27 17:39:05 +0800

    后台角色应用

--- Admin/Controller/RoleAppController.class.php
+++ Admin/Controller/RoleAppController.class.php
@@ -14,9 +14,13 @@ class RoleAppController extends BaseController {
     
     //添加角色应用
     public function add(){
+        if(IS_POST){
+            dump($_REQUEST);exit;
+        }
         $roleModel = M('Role');//检索所有角色
         $roles = $roleModel->select();
         $role_select = array();
+        $apps_arr = array();
         if(!empty($roles)){
             foreach($roles as $role){
                 $role_select[]= array('role_id'=>$role['id'],'role_name'=>$role['name']);
@@ -25,8 +29,22 @@ class RoleAppController extends BaseController {
         $appModel = M('App');//检索所有应用
         $apps = $appModel->select();
         $this->assign('apps',$apps);
+        if(!empty($apps)){
+            foreach($apps as $app){
+                $apps_arr[] = array('app_id'=>$app['id'],'app_content'=>$app['url'].' | '.$app['name'],'checked'=>false);
+            }
+        }
+        $this->assign('apps_json',  json_encode($apps_arr));
         $this->assign('role_select',  json_encode($role_select));
         $this->display('add');
+    }
+    
+    //选择角色已经拥有的应用
+    public function choiceApps(){
+        if(IS_POST){
+//            $apps[] = array('app_id'=>4,'app_content'=>'test','checked'=>true);
+//            $this->ajaxReturn(json_encode($apps));
+        }
     }
     
     //编辑角色应用

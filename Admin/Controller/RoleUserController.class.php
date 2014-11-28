770ec92d2a4e01a6475e5550f42f644d369d7123
tonbo <tonbochow@qq.com>
2014-11-28 23:21:50 +0800

    后台用户角色分配修改

--- Admin/Controller/RoleUserController.class.php
+++ Admin/Controller/RoleUserController.class.php
@@ -21,9 +21,27 @@ class RoleUserController extends BaseController {
         }
         $roleUserModel = M('RoleUser');
         $role_user = $roleUserModel->where(array('user_id'=>array('between',$user_ids)))->select();
-        dump($role_user);
+        $user_arr = array();
+        if(!empty($role_user)){
+            foreach($role_user as $roleUser){
+                if(isset($user_arr[$roleUser['user_id']])){
+                    $user_arr[$roleUser['user_id']]['role_id'] .= ','.$roleUser['role_id'];
+                    $user_arr[$roleUser['user_id']]['role_name'] .= ','.$roleUser['role_name'];
+                }else{
+                    $user_arr[$roleUser['user_id']]['role_id'] = $roleUser['role_id'];
+                    $user_arr[$roleUser['user_id']]['role_name'] =  $roleUser['role_name'];
+                }
+            }
+        }
+        if(!empty($users)){
+            foreach($users as $key=>$user){
+                $users[$key]['role_ids'] = $user_arr[$user['id']]['role_id'];
+                $users[$key]['role_names'] = $user_arr[$user['id']]['role_name'];
+            }
+        }
         
         $show = $Page->show(); // 分页显示输出
+        $this->assign('users',$users);
         $this->assign('page', $show);
         $this->display('index');
     }
